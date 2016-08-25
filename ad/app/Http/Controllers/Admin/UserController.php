4b86140682f71afe5df1f1131fc7393a196241f5
andyYu <yuxianjun001@icloud.com>
2016-08-25 10:53:49 +0800

    first commit

---
+++ ad/app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class UserController extends BaseController
+{
+    public function login(){
+        echo 123;
+    }
+}
